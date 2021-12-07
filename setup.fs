615fd41804bf3fd941fa0acedc821d539807454f
Silas Vedder <singingfox123@protonmail.com>
2021-12-07 14:52:53 +0100

    Split up word

--- setup.fs
+++ setup.fs
@@ -1,6 +1,11 @@
-: newgame!
-  0 head ! xdim 2 / ydim 2 / snake 2! 3 3 apple 2! 3 length !
-  ['] up direction ! left step! left step! left step! left step! ;
+: reset-head       0 head ! ;
+: reset-length     3 length ! ;
+: reset-pos        xdim 2 / ydim 2 / snake 2! ;
+: reset-snake      reset-head reset-pos reset-length ;
+: reset-apple      3 15 myrand 3 15 myrand apple 2! ;
+: reset-direction  ['] up direction ! ;
+
+: newgame!  reset-snake reset-apple reset-direction ;
 
 : vi?      page ." Vi keys? (y/n)" key 121 = vi? ! ;
 : greet    page ." Snake in Forth" ;
